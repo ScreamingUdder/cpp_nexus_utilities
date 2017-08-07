120f232de5d7beccd9731d7c8550acf4b3476a62
Michael Turner <michaeljturner@live.com>
2017-08-07 15:31:06 +0100

    Now calculating pixels offsets for all detectors

---
+++ detectorplotter.h
@@ -0,0 +1,44 @@
+﻿#ifndef DETECTORPLOTTER_H
+#define DETECTORPLOTTER_H
+
+#include "H5Cpp.h"
+#include "Eigen/Core"
+#include "Eigen/Geometry"
+
+#include <vector>
+
+namespace Nexus
+{
+    const H5std_string NX_ENTRY = "NXentry";
+    const H5std_string NX_INSTRUMENT = "NXinstrument";
+    const H5std_string NX_DETECTOR = "NXdetector";
+}
+
+class DetectorPlotter
+{
+public:
+    //Constructor for DetectorPlotter class
+    DetectorPlotter(H5std_string FILE_NAME);
+    //Destructor for DetectorPlotter class
+    ~DetectorPlotter();
+    //Stores all detector groups in a vector
+    std::vector<H5::Group> openDetectorGroups();
+    //Stores detectorGroup pixel offsets as Eigen 3xN matrix
+    Eigen::Matrix<double, 3, Eigen::Dynamic> getPixelOffsets(H5::Group &detectorGroup);
+    //Gets the transformations applied to the detector's pixelOffsets
+    Eigen::Transform<double, 3, Eigen::Affine> getTransformations(H5::Group &detectorGroup);
+    //Gets the data from a string dataset
+    H5std_string get1DStringDataset(H5std_string &dataset);
+
+private:
+    //Nexus file
+    H5::H5File file;
+    //Root group for nexus file
+    H5::Group rootGroup;
+    //Read dataset into vector
+    template<typename valueType> std::vector<valueType> get1DDataset(H5std_string &dataset);
+    //Opens sub groups matching CLASS_TYPE
+    std::vector<H5::Group> openSubGroups(H5::Group &parentGroup, H5std_string CLASS_TYPE);
+};
+
+#endif // DETECTORPLOTTER_H
---
+++ main.cpp
@@ -0,0 +1,37 @@
+﻿#include "detectorplotter.h"
+
+#include "H5Cpp.h"
+#include "Eigen/Core"
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace H5;
+typedef Eigen::Matrix<double, 3, Eigen::Dynamic> Pixels;
+
+int main(int argc, char *argv[])
+{
+    H5std_string FILE_NAME;
+    if (argc > 1)
+    {
+        FILE_NAME = argv[1];
+    }
+
+    DetectorPlotter nexusFile(FILE_NAME);
+    // Get path to all detector groups
+    std::vector<Group> detectorGroups = nexusFile.openDetectorGroups ();
+
+    for(std::vector<Group>::iterator iter = detectorGroups.begin (); iter < detectorGroups.end (); iter++)
+    {
+        // Get the pixel offsets
+        Pixels pixelOffsets = nexusFile.getPixelOffsets (*iter);
+
+        // Get the transformations
+        Eigen::Transform<double, 3,2> transforms = nexusFile.getTransformations (*iter);
+
+        // Detector Pixels
+        Pixels detectorPixels = transforms * pixelOffsets;
+    }
+}
